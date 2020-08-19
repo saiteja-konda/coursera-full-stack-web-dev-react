2b20a75062cb119f2b585fd5e0f5d6a177ecec2c
saiteja <kondasaitej@protonmail.com>
2020-08-20 00:02:02 +0530

    components-part-2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="dark">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/menuComponent";
+import { DISHES } from "./shared/dishes";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
